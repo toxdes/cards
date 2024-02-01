9f772b6958039c16913473097106c7cf3ea65e11
toxdes <malivp3494@gmail.com>
2024-02-02 03:13:32 +0530

    fix: create github release script

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eux pipefail
-# source ./.env
+source ./.env
 # echo $GH_TOKEN
 APP_VERSION=$(yq -r '.version' ./pubspec.yaml | sed -e 's/\+/_/')
 # flutter build apk
-RELEASE_NAME="cards-${APP_VERSION}"
+RELEASE_NAME="cards-release-${APP_VERSION}"
 echo "[INFO] Generating source tar.gz"
 git archive --format=tar.gz -o /tmp/cards.tar.gz --prefix=cards/ main
 echo "[INFO] Building $RELEASE_NAME.apk"
@@ -22,12 +22,12 @@ CREATE_RELEASE_RES=$(curl -L \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/toxdes/cards/releases \
-  -d "{\"tag_name\":\"internal\",\"target_commitish\":\"main\",\"name\":\"$RELEASE_NAME\",\"body\":\"test\",\"draft\":true,\"prerelease\":false,\"generate_release_notes\":false}")
+  -d "{\"tag_name\":\"internal\",\"target_commitish\":\"main\",\"name\":\"$RELEASE_NAME\",\"body\":\"## Release Notes\n This is currently work in progress, more detailed release notes will be added later.\",\"draft\":false,\"prerelease\":false,\"generate_release_notes\":false}")
 RELEASE_ID=$(echo $CREATE_RELEASE_RES | jq -r '.id')
 echo "RELEASE ID: $RELEASE_ID"
 
 # upload assets
-echo "[INFO] Uploading assets"
+echo "[INFO] Uploading APK"
 UPLOAD_URL="https://uploads.github.com/repos/toxdes/cards/releases/$RELEASE_ID/assets?name=$RELEASE_NAME.apk&label=$RELEASE_NAME.apk"
 curl -L \
   -X POST \
@@ -37,3 +37,14 @@ curl -L \
   -H "Content-Type: application/octet-stream" \
   "$UPLOAD_URL" \
   --data-binary "@$APK_PATH"
+
+echo "[INFO] Uploading source tarball"
+UPLOAD_URL="https://uploads.github.com/repos/toxdes/cards/releases/$RELEASE_ID/assets?name=cards.tar.gz&label=Source%20code%20(tarball)"
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Content-Type: application/octet-stream" \
+  "$UPLOAD_URL" \
+  --data-binary "@/tmp/cards.tar.gz"
