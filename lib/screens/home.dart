f38034f23f20137a53a8e953c8884f3eaea87101
toxdes <malivp3494@gmail.com>
2024-02-27 14:05:54 +0530

    feat: add maxWidth, center layout for large screens

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -50,89 +50,92 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-          decoration: const BoxDecoration(color: ThemeColors.gray1),
-          child: Stack(textDirection: TextDirection.ltr, children: [
-            Container(
-                padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
-                child: Directionality(
-                    textDirection: TextDirection.ltr,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text("Saved cards (${_cards.length})",
-                                  textAlign: TextAlign.left,
-                                  style: const TextStyle(
-                                      fontFamily: Fonts.rubik,
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.w600,
-                                      color: ThemeColors.white2)),
-                            ]),
-                        const SizedBox(height: 12),
-                        Expanded(
-                            child: ListView(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          children: _cards.length == 0
-                              ? [
-                                  Container(
-                                      margin: const EdgeInsets.fromLTRB(
-                                          16, 48, 16, 32),
-                                      child: const Text(
-                                          "You haven't added any cards yet.",
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                              fontFamily: Fonts.rubik,
-                                              fontWeight: FontWeight.w400,
-                                              color: ThemeColors.white2)))
-                                ]
-                              : _cards.getAll().map((CardModel card) {
-                                  return CardView(
-                                      card: card,
-                                      onLongPress: (CardModel c) {
-                                        removeCard(c);
-                                      });
-                                }).toList(),
-                        )),
-                        Button(
-                          text: "Add new card +",
-                          buttonType: ButtonType.primary,
-                          alignment: Alignment.center,
-                          height: 48,
-                          onTap: () {
-                            setState(() {
-                              _addNewCardFormVisible = true;
-                            });
-                          },
-                        )
-                            .animate(
-                                autoPlay: true,
-                                onComplete: (controller) {
-                                  controller.loop(count: 3);
-                                })
-                            .then(delay: 10.seconds)
-                            .shake(
-                              duration: 600.ms,
-                              rotation: 0.02,
-                            ),
-                      ],
-                    ))),
-            AddNewCardModal(
-              isVisible: _addNewCardFormVisible,
-              onClose: () {
-                setState(() {
-                  _addNewCardFormVisible = false;
-                });
-              },
-              onAddNewCard: addCard,
-            ),
-          ])),
+      child: Center(
+        child: Container(
+            decoration: const BoxDecoration(color: ThemeColors.gray1),
+            constraints: const BoxConstraints(maxWidth: 800),
+            child: Stack(textDirection: TextDirection.ltr, children: [
+              Container(
+                  padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
+                  child: Directionality(
+                      textDirection: TextDirection.ltr,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text("Saved cards (${_cards.length})",
+                                    textAlign: TextAlign.left,
+                                    style: const TextStyle(
+                                        fontFamily: Fonts.rubik,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600,
+                                        color: ThemeColors.white2)),
+                              ]),
+                          const SizedBox(height: 12),
+                          Expanded(
+                              child: ListView(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            children: _cards.length == 0
+                                ? [
+                                    Container(
+                                        margin: const EdgeInsets.fromLTRB(
+                                            16, 48, 16, 32),
+                                        child: const Text(
+                                            "You haven't added any cards yet.",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                fontFamily: Fonts.rubik,
+                                                fontWeight: FontWeight.w400,
+                                                color: ThemeColors.white2)))
+                                  ]
+                                : _cards.getAll().map((CardModel card) {
+                                    return CardView(
+                                        card: card,
+                                        onLongPress: (CardModel c) {
+                                          removeCard(c);
+                                        });
+                                  }).toList(),
+                          )),
+                          Button(
+                            text: "Add new card +",
+                            buttonType: ButtonType.primary,
+                            alignment: Alignment.center,
+                            height: 48,
+                            onTap: () {
+                              setState(() {
+                                _addNewCardFormVisible = true;
+                              });
+                            },
+                          )
+                              .animate(
+                                  autoPlay: true,
+                                  onComplete: (controller) {
+                                    controller.loop(count: 3);
+                                  })
+                              .then(delay: 10.seconds)
+                              .shake(
+                                duration: 600.ms,
+                                rotation: 0.02,
+                              ),
+                        ],
+                      ))),
+              AddNewCardModal(
+                isVisible: _addNewCardFormVisible,
+                onClose: () {
+                  setState(() {
+                    _addNewCardFormVisible = false;
+                  });
+                },
+                onAddNewCard: addCard,
+              ),
+            ])),
+      ),
     );
   }
 }
