b5772083a53e3cdd4b2c1727dd7241174165ec5a
toxdes <malivp3494@gmail.com>
2024-05-11 22:18:26 +0530

    feat: add build info to sentry logs

--- lib/services/sentry_service.dart
+++ lib/services/sentry_service.dart
@@ -1,3 +1,4 @@
+import 'package:cards/services/package_info_service.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
@@ -6,11 +7,16 @@ class SentryService {
   static NavigatorObserver? _navigationObserver;
 
   static Future<void> init() async {
-    await SentryFlutter.init((options) {
-      options.dsn =
-          "https://55a155db5f0c6120375c23a4fc4799f8@o4507236696326144.ingest.us.sentry.io/4507236719198208";
+    await SentryFlutter.init((options) async {
+      PackageInformation info = await PackageInfoService.getPackageInfo();
+      options
+        ..dsn =
+            "https://55a155db5f0c6120375c23a4fc4799f8@o4507236696326144.ingest.us.sentry.io/4507236719198208"
+        ..release = '${info.versionName}-${info.versionCode}'
+        ..sampleRate = 0.2
+        ..environment = info.env;
+      _initialized = true;
     });
-    _initialized = true;
   }
 
   static Future<void> error(throwable, StackTrace stackTrace) async {
