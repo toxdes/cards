6ebc0e00f4cb09a7e4452f72c7beeb99cecfadae
toxdes <malivp3494@gmail.com>
2024-09-21 07:58:34 +0530

    feat: updgrade dependencies

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,12 +5,12 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   flutter_secure_storage_windows
-  sentry_flutter
   share_plus
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  sentry_flutter
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
