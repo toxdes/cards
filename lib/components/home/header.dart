560dd0942fd82a24093423b4a9cf6eee368fc68f
toxdes <malivp3494@gmail.com>
2025-11-22 00:29:46 +0530

    feat: introduce ui scaffolds for preferences

--- lib/components/home/header.dart
+++ lib/components/home/header.dart
@@ -3,6 +3,7 @@ import 'package:cards/components/shared/icon_button.dart';
 import 'package:cards/config/colors.dart';
 import 'package:cards/config/fonts.dart';
 import 'package:cards/screens/backup_restore/backup_main.dart';
+import 'package:cards/screens/preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide IconButton;
@@ -17,12 +18,20 @@ class Header extends StatelessWidget {
             builder: (context) => const BackupMainScreen()));
   }
 
+  void navigateToPreferences(BuildContext context) {
+    Navigator.push(
+        context,
+        CupertinoPageRoute(
+            title: "Preferences",
+            builder: (context) => const PreferencesScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       // color: ThemeColors.red,
       padding: EdgeInsets.fromLTRB(0, 12, 0, 0),
-      child: Stack(children: [
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         Container(
           alignment: Alignment.centerLeft,
           child: Text("Saved cards",
@@ -37,14 +46,27 @@ class Header extends StatelessWidget {
             ? const SizedBox.shrink()
             : Container(
                 alignment: Alignment.centerRight,
-                child: IconButton(
-                    onTap: () {
-                      navigateToBackupRestore(context);
-                    },
-                    size: 28,
-                    color: ThemeColors.white1,
-                    buttonType: ButtonType.primary,
-                    iconData: Icons.backup_rounded))
+                child: Row(
+                  children: [
+                    IconButton(
+                        onTap: () {
+                          navigateToBackupRestore(context);
+                        },
+                        size: 28,
+                        color: ThemeColors.white1,
+                        buttonType: ButtonType.primary,
+                        iconData: Icons.backup_outlined),
+                    SizedBox(width: 8),
+                    IconButton(
+                        onTap: () {
+                          navigateToPreferences(context);
+                        },
+                        size: 28,
+                        color: ThemeColors.white1,
+                        buttonType: ButtonType.primary,
+                        iconData: Icons.tune_outlined),
+                  ],
+                ))
       ]),
     );
   }
